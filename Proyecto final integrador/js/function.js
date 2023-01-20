0eac91e9987d6172348f4286b5ebb020fe955b61
luis casas <lukacontacto@gmail.com>
2023-01-20 08:54:43 -0300

    primer versionado

---
+++ Proyecto final integrador/js/function.js
@@ -0,0 +1,13 @@
+
+document.getElementById("insta").onclick = function () {
+    console.log("Clik en boton Insta");
+    document.getElementById("pantalla").innerHTML = "@soylucho en Instagram";
+}
+
+document.getElementById("face").onclick = function() {
+    console.log("Clik en boton Face");
+    document.getElementById("pantalla").innerHTML = "SoyLucho en Facebook";
+}
+
+
+
