7255d576f022bca7b2986c12b3d377d55479fbf7
luis casas <lukacontacto@gmail.com>
2023-02-25 14:52:25 -0300

    Dark Mod Integrado

--- js/function.js
+++ js/function.js
@@ -10,4 +10,17 @@ document.getElementById("face").onclick = function() {
 }
 
 
-
+/*Si clicamos en el botón del sol, borrarémos la clase css dark-mode del div 
+con id general y se aplicará el estilo active al sol*/
+document.getElementById('id-sun').onclick = function(){
+    document.getElementById('gral').classList.remove('dark-mode')
+    document.getElementById('id-moon').classList.remove('active')
+    this.classList.add('active')
+  }
+  /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del div 
+  con id general y se aplicará el estilo active a la luna*/
+  document.getElementById('id-moon').onclick = function(){
+    document.getElementById('gral').classList.add('dark-mode')
+    document.getElementById('id-sun').classList.remove('active')
+    this.classList.add('active')
+  }
